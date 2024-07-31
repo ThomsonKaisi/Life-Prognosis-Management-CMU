6bc24929cad3c31110a93ba4c42c2d3497c67e8a
Thomson Kaisi <thomsonkaisi@gmail.com>
2024-07-31 22:21:42 +0200

    Adding Authentication

--- usermanagement/Account.java
+++ usermanagement/Account.java
@@ -1,5 +1,14 @@
 package usermanagement;
-
+import java.util.Date;
 public class Account {
-    
+    private Date createdOn;
+    private String accountType;
+    private Boolean isActive;
+    private String passwordHash;
+    private Admin createdBy;
+    private int accountID;
+
+Account(){
+
+}
 }
--- usermanagement/Authentication.java
+++ usermanagement/Authentication.java
@@ -2,4 +2,14 @@ package usermanagement;
 
 public class Authentication {
     
+Authentication(){
+
+}
+public String logIn(String password,String email){
+
+return token
+}
+private boolean VerifyEmail(String email){
+
+}
 }
